67901986a618ecd31463d24f4661950a67adaef7
lkportal <lksxim@gmail.com>
2023-09-14 17:53:42 -0300

    add profolio

---
+++ src/pages/Menus.js
@@ -0,0 +1,9 @@
+import Contato from "./contato"
+import Link from "next/link"
+export default function Menus(){
+    return(
+        <div>
+            <Link href="/contato">Contato</Link>
+        </div>
+    )
+}
